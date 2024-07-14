993f20411b67df4d9a421d4d5fa3326f81cd5c6d
Zach Saunders <zachisaunders12@gmail.com>
2024-07-14 11:24:00 -0400

    Initial commit

---
+++ lua/personal/init.lua
@@ -0,0 +1,4 @@
+require("personal.remap")
+require("personal.packer")
+require("personal.set")
+require("autoclose").setup()
---
+++ lua/personal/set.lua
@@ -0,0 +1,16 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.smartcase = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.ruler = true
