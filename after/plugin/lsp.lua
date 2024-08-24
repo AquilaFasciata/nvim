776d405f0442c80f816f3394cc29d1154ed927f4
Zach Saunders <zachisaunders12@gmail.com>
2024-08-23 22:48:50 -0400

    Added tabs, case coercion, and some lsp nonsense

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,3 +18,9 @@ require('mason-lspconfig').setup({
     end,
   },
 })
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
