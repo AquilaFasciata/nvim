36c6d000d89eb3b7cd07671e4a7dc719b5e07329
Zach Saunders <zachisaunders12@gmail.com>
2024-09-22 15:57:28 -0400

    Rust Auto-use support

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -3,38 +3,35 @@ local cmp = require("cmp")
 
 cmp.setup({
 	snippet = {
-    expand = function(args)
+		expand = function(args)
 			require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = {
-    ["<Enter>"] = cmp.mapping(function(fallback)
-      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-      if cmp.visible() then
-        local entry = cmp.get_selected_entry()
-        if not entry then
-          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-        end
-        cmp.confirm()
-      else
-        fallback()
-      end
-    end, {"i","s","c",}),
+		end,
+	},
+	mapping = {
+		["<Enter>"] = cmp.mapping(function(fallback)
+			-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+			if cmp.visible() then
+				local entry = cmp.get_selected_entry()
+				if not entry then
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				end
+				cmp.confirm()
+			else
+				fallback()
+			end
+		end, { "i", "s", "c", }),
 		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-k>"] = cmp.mapping.select_prev_item(),
-  },
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
+	},
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
 		{ name = "nvim_lsp_signature_help" },
-    { name = "buffer" },
-		{ name = 'luasnip', option = { show_autosnippets = true } },
-  }),
+		{ name = "buffer" },
+		{ name = 'luasnip',                option = { show_autosnippets = true } },
+	}),
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-
 cmp.event:on(
-'confirm_done',
-cmp_autopairs.on_confirm_done()
+	'confirm_done',
+	cmp_autopairs.on_confirm_done()
 )
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,30 +1,33 @@
 local lsp_zero = require('lsp-zero')
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 local lsp_attach = function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+	lsp_zero.default_keymaps({ buffer = bufnr })
 	lsp_zero.buffer_autoformat()
 end
 
 lsp_zero.extend_lspconfig({
-  lsp_attach = lsp_attach,
+	lsp_attach = lsp_attach,
 })
 
 
 require("luasnip.loaders.from_vscode").lazy_load()
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  -- Replace the language servers listed here 
-  -- with the ones you want to install
-  ensure_installed = {'ts_ls', 'rust_analyzer', 'gopls'},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+	-- Replace the language servers listed here
+	-- with the ones you want to install
+	ensure_installed = { 'ts_ls', 'rust_analyzer', 'gopls' },
+	handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup({})
+		end,
+	},
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-require'lspconfig'.cssls.setup {
-  capabilities = capabilities,
+require 'lspconfig'.cssls.setup {
+	capabilities = capabilities,
+}
+require('lspconfig').rust_analyzer.setup {
+	capabilities = capabilities,
 }
