ea0600fa90abe8c633dad098edc96fa13b106f60
Zach <zachisaunders12@gmail.com>
2024-12-15 14:02:30 -0500

    Tweaking colors

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -2,6 +2,7 @@ function SetMyColors(color)
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "BlameLineNvim", { bg = "none" })
 end
 
 SetMyColors("tokyonight")
