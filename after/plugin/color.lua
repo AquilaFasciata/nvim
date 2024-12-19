7ce9da64d39596335b7b44e62d0d7bac7bf607ec
Zach Saunders <zachisaunders12@gmail.com>
2024-12-19 07:30:44 -0500

    Transparency for splits

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,6 +1,7 @@
 function SetMyColors(color)
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	vim.api.nvim_set_hl(0, "BlameLineNvim", { bg = "none" })
 end
