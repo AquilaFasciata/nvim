a80c80a158af88c5fb6ab4597d4a8759bc9a0ccd
Zach Saunders <zachisaunders12@gmail.com>
2024-12-28 23:18:55 -0500

    Make line numbers easier to read

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -4,6 +4,9 @@ function SetMyColors(color)
 	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	vim.api.nvim_set_hl(0, "BlameLineNvim", { bg = "none" })
+	vim.api.nvim_set_hl(0, "LineNr", { fg = "white" })
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#77a1dc" })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#77a1dc" })
 end
 
 SetMyColors("tokyonight")
