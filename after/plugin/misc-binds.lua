e61f404209b65d68e63f13b6f3dd04eae872b1ed
Zach <zachisaunders12@gmail.com>
2026-08-22 12:07:34 -0400

    Fold toggle and some descriptions

--- after/plugin/misc-binds.lua
+++ after/plugin/misc-binds.lua
@@ -1,3 +1,26 @@
 vim.keymap.set("n", "<leader>lpm", vim.cmd.Lazy)
 vim.keymap.set("n", "<leader>cc", vim.cmd.make)
-vim.keymap.set("n", "<leader>fr", vim.cmd.set({ args = { 'foldlevel=99' } }))
+
+vim.keymap.set("n", "<leader>fr", function()
+	vim.cmd.set({ args = { 'foldlevel=99' } })
+end, { desc = 'Reset fold level (Sets to 99)' })
+
+vim.keymap.set("n", "<leader>ft", function()
+	if FOLD_RESET_EXECUTED == nil then
+		FOLD_RESET_EXECUTED = false
+		FR_NEXTFOLD = -1
+	end
+
+	FR_LASTFOLD = vim.o.foldlevel
+
+	vim.notify('Last fold: ' .. FR_LASTFOLD)
+
+	if FOLD_RESET_EXECUTED then
+		vim.o.foldlevel = FR_NEXTFOLD
+	else
+		vim.o.foldlevel = 99
+		FOLD_RESET_EXECUTED = true
+	end
+
+	FR_NEXTFOLD = FR_LASTFOLD
+end, { desc = 'Toggles foldlevel between 2 levels' })
