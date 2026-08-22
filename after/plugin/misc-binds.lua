3264838ade3b58aaee0bdb64f7a7cfce14d7f7b4
Zach <zachisaunders12@gmail.com>
2026-08-22 11:05:51 -0400

    Install vimwiki, foldlevel reset keybind

--- after/plugin/misc-binds.lua
+++ after/plugin/misc-binds.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>lpm", vim.cmd.Lazy)
 vim.keymap.set("n", "<leader>cc", vim.cmd.make)
+vim.keymap.set("n", "<leader>fr", vim.cmd.set({ args = { 'foldlevel=99' } }))
---
+++ lua/plugins/vimwiki.lua
@@ -0,0 +1,6 @@
+return {
+	'vimwiki/vimwiki',
+	init = function()
+		vim.g.vimwiki_path = '~/.vimwiki'
+	end
+}
